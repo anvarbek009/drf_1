1e81895510c1581f7b95b68540fa1199777e2eb3
anvarbek009 <anvarasqarov2009@gmail.com>
2024-05-25 22:46:00 +0500

    commit

--- signs/views.py
+++ signs/views.py
@@ -16,3 +16,25 @@ class ListCategoryApiView(APIView):
             'status_code':status.HTTP_200_OK
         }
         return Response(serializer_data)
+    
+class ListRoadSignsApiView(APIView):
+    def get(self,request):
+        model=RoadSigns.objects.all()
+        serializer=RoadSignsSerializer(model,many=True)
+        serializer_data={
+            'data':serializer.data,
+            'status':'success',
+            'status_code':status.HTTP_200_OK
+        }
+        return Response(serializer_data)
+    
+class DetailRoadSignsApiView(APIView):
+    def get(self,request,pk):
+        model=RoadSigns.objects.get(id=pk)
+        serializer=RoadSignsSerializer(model)
+        serializer_data={
+            'data':serializer.data,
+            'status':'success',
+            'status_code':status.HTTP_200_OK
+        }
+        return Response(serializer_data)
